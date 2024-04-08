c1164499d50ffc6d61b3fb44eb55cadd0ada116f
BriannaViz <146958013+BriannaViz@users.noreply.github.com>
2024-04-08 13:56:03 +0000

    uh

--- compoundConditionals.js
+++ compoundConditionals.js
@@ -2,26 +2,36 @@
 
 // 1. Create a secret username and password.
 //  - The variables are declared already. Update the values.
-let secretUsername = ("");
-let secretPassword = ("");
+let secretUsername = ("hi");
+let secretPassword = ("hi");
 let button = document.querySelector("button");
 
 button.addEventListener("click", function() {
-    let username = document.querySelector(".username").value;
-    let password = document.querySelector(".password").value;
-    let message = document.querySelector(".message");
+  let username = document.querySelector(".username").value;
+  let password = document.querySelector(".password").value;
+  let message = document.querySelector(".message");
 
-    // 2. Create a compound conditional statement to check for the following:
-    //  - If the username AND password is correct, update the text of the message div to say "Success!"
-    
-    // CODE SOLO
-    // 3. Create additional compound conditionals that check for the following:
-    //  - If the username is correct AND the password is incorrect, update the text of the message div to say "Wrong password - be careful, don't mess this up too many times."
-    //  - If the password is correct AND the username is incorrect, update the text of the message div to say "Wrong username - is there a typo?"
-    //  - Finally, if the password is incorrect AND the username is incorrect, update the text of the message div to say "Incorrect username and password. Hmm. Suspicious."
-    
-    
-    // Bonus: If the username OR password fields are empty, update the text of the message div to say "You need to enter SOMETHING."
-    //  - Hint: What string represents an "empty" message.
-    
+  // 2. Create a compound conditional statement to check for the following:
+  if (username === secretUsername && password === secretPassword) {
+    message.innerHTML = "success"
+  } else if (username === secretUsername && password !== secretPassword) {
+    message.innerHTML = "Wrong password, be careful, don't mess this up too many times."
+  } else if (username !== secretUsername && password === secretPassword) {
+    message.innerHTML = "Wrong username - is there a typo?"
+  } else {
+  message.innerHTML = "Incorrect username and password. Hmm. Suspicious."}
+
+
+  //  - If the username AND password is correct, update the text of the message div to say "Success!"
+
+  // CODE SOLO
+  // 3. Create additional compound conditionals that check for the following:
+  //  - If the username is correct AND the password is incorrect, update the text of the message div to say "Wrong password - be careful, don't mess this up too many times."
+  //  - If the password is correct AND the username is incorrect, update the text of the message div to say "Wrong username - is there a typo?"
+  //  - Finally, if the password is incorrect AND the username is incorrect, update the text of the message div to say "Incorrect username and password. Hmm. Suspicious."
+
+
+  // Bonus: If the username OR password fields are empty, update the text of the message div to say "You need to enter SOMETHING."
+  //  - Hint: What string represents an "empty" message.
+
 });
